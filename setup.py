c0847db5af9912ba658677e44b7e36c32d87707b
Tarek Ziade <tarek@ziade.org>
2019-10-09 11:17:26 +0200

    skeleton

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+install_requires = []
+
+
+setup(
+    name="chglg",
+    version="0.1",
+    packages=find_packages(),
+    description="Unified Changelog",
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points="""
+      [console_scripts]
+      chglg-collector = chglg.collector:main
+      chglg-web = chglg.web:main
+      """,
+)
